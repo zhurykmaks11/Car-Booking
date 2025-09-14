ef01048c9c22172b52f4e1f550eff38a5998fcd8
zhurykmaks11 <zhurik.max@gmail.com>
2025-09-14 20:55:09 +0300

    add Getter and Setter and base information about Booking

--- src/main/java/com/example/demo/model/Booking.java
+++ src/main/java/com/example/demo/model/Booking.java
@@ -1,4 +1,19 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+@Getter
+@Setter
+@Component
 public class Booking {
+    private String id;
+    private User user;
+    private Car car;
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private double totalPrice;
+    private String status;
 }
