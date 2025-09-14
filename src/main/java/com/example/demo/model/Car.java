26865538dd193aecdb679142c306c8c6fa6b8950
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-09-14 20:26:06 +0300

    Add CarService; CarController base structure

--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class Car implements Cloneable {
     private String id;
     private String brand;
