083e8cfc116692f8ce942c253f7eea31777a67f4
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-09-14 20:25:13 +0300

    Add Car model(add id line to User model)

---
+++ src/main/java/com/example/demo/model/Car.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Car implements Cloneable {
+    private String id;
+    private String brand;
+    private String model;
+    private int year;
+    private int seats;
+    private String fuelType;
+    private String transmission;
+    private double pricePerDay;
+    private boolean available = true;
+
+    @Override
+    public Car clone() {
+        try {
+            Car copy = (Car) super.clone();
+            System.out.println("[Prototype] Cloned car: " + this.brand + " " + this.model);
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 public class User {
+    private String id;
     private String name;
     private String email;
     private String phone;
