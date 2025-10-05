75e8bc8ddc0c18052235deb908a92f242f9fb5b6
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-10-05 19:20:36 +0300

    Create methods in Main for rearranging pattern demonstrations

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,6 +20,20 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+		System.out.println("===== Testing Abstract Factory Pattern =====");
+		testAbstractFactory();
+
+		System.out.println("\n===== Testing Factory Method Pattern =====");
+		testFactoryMethod();
+
+		System.out.println("\n===== Testing Builder Pattern =====");
+		testBuilderMethod();
+
+		System.out.println("\n===== Testing Object Pool Pattern =====");
+		testObjectPool();
+	}
+
+	private static void testAbstractFactory() {
 		RentalFactory factory = new EuropeRentalFactory();
 
 		InsurancePolicy insurance = factory.createInsurance();
@@ -27,8 +41,9 @@ public class DemoApplication {
 
 		insurance.insure();
 		requirements.checkRequirements();
+	}
 
-		// Перевірка Factory Method
+	private static void testFactoryMethod() {
 		BookingCreator hourlyCreator = new HourlyBookingCreator();
 		Booking hourlyBooking = hourlyCreator.processBooking(5);
 		System.out.println(hourlyBooking);
@@ -36,17 +51,17 @@ public class DemoApplication {
 		BookingCreator dailyCreator = new DailyBookingCreator();
 		Booking dailyBooking = dailyCreator.processBooking(3);
 		System.out.println(dailyBooking);
+	}
 
-		//Bilder test
-		User testUser = new User(); // у тебе є модель User
+	private static void testBuilderMethod() {
+		User testUser = new User();
 		testUser.setName("Maxim");
 
-		Car testCar = new Car(); // у тебе є модель Car
+		Car testCar = new Car();
 		testCar.setModel("Tesla Model 3");
 
 		BookingDirector director = new BookingDirector();
 
-		// Створимо бронювання на години
 		ConcreteBooking hourlyBuilt = director.createHourlyBooking(
 				testUser,
 				testCar,
@@ -58,7 +73,6 @@ public class DemoApplication {
 				" booked " + hourlyBuilt.getCar().getModel() +
 				" for " + hourlyBuilt.getStatus());
 
-		// Створимо бронювання на дні
 		ConcreteBooking dailyBuilt = director.createDailyBooking(
 				testUser,
 				testCar,
@@ -69,8 +83,9 @@ public class DemoApplication {
 		System.out.println("Builder -> " + dailyBuilt.getUser().getName() +
 				" booked " + dailyBuilt.getCar().getModel() +
 				" for " + dailyBuilt.getStatus());
+	}
 
-		//------------------Object Pool ---------------------------
+	private static void testObjectPool() {
 		CarPool pool = new CarPool();
 
 		Car car1 = pool.getCar();
@@ -79,7 +94,6 @@ public class DemoApplication {
 		Car car2 = pool.getCar();
 		System.out.println("Отримано авто 2: " + car2.getBrand() + " " + car2.getModel());
 
-		// Спроба взяти ще одне авто (пул порожній)
 		Car car3 = pool.getCar();
 		if (car3 == null) {
 			System.out.println("Немає доступних автомобілів у пулі!");
@@ -88,11 +102,9 @@ public class DemoApplication {
 		pool.release(car1);
 		System.out.println("Повернено авто 1 назад у пул.");
 
-		// Тепер знову можна взяти один авто
 		Car car4 = pool.getCar();
 		System.out.println("Отримано авто 4: " + car4.getBrand() + " " + car4.getModel());
 
-		// Перевіримо, чи це той самий об’єкт, що й car1
 		System.out.println("car1 == car4 ? " + (car1 == car4));
 	}
 }
